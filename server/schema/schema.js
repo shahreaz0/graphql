c334ea6ac352cb34d64c99203d7f134b36b3d3d0
shahreaz <ashahreaz@gmail.com>
2021-08-24 01:54:08 +0600

    Gql id, make author type and set the type relation between author and books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,18 +1,49 @@
 const graphgl = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphgl;
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLSchema,
+	GraphQLID,
+	GraphQLInt,
+} = graphgl;
 
 let books = [
-	{ name: "Name of the wind", genre: "Fantasy", id: "1" },
-	{ name: "The final empire", genre: "Fantasy", id: "2" },
-	{ name: "The long earth", genre: "Sci-Fi", id: "3" },
+	{ name: "Name of the wind", genre: "Fantasy", id: "1", authorId: "1" },
+	{ name: "The final empire", genre: "Fantasy", id: "2", authorId: "2" },
+	{ name: "The long earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+];
+
+let authors = [
+	{ name: "Patrick Rothfuss", age: 44, id: "1" },
+	{ name: "Brandon Sanderson", age: 42, id: "2" },
+	{ name: "Terry Pratchett", age: 66, id: "3" },
 ];
 
 const BookType = new GraphQLObjectType({
 	name: "Book",
 	fields: () => ({
-		id: { type: GraphQLString },
+		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString },
+		author: {
+			type: AuthorType,
+			resolve(parent, args) {
+				const data = authors.filter(
+					(author) => author.id === parent.id
+				);
+
+				return data[0];
+			},
+		},
+	}),
+});
+
+const AuthorType = new GraphQLObjectType({
+	name: "Author",
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt },
 	}),
 });
 
@@ -21,10 +52,17 @@ const RootQuery = new GraphQLObjectType({
 	fields: {
 		book: {
 			type: BookType,
-			args: { id: { type: GraphQLString } },
+			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				const data = books.filter((book) => book.id === args.id);
-				// console.log(data);
+				return data[0];
+			},
+		},
+		author: {
+			type: AuthorType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				const data = authors.filter((author) => author.id !== args.id);
 				return data[0];
 			},
 		},
