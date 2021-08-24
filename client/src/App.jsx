81f9492bbbe78a5b9028a516e259b011f4bdb5e8
shahreaz <ashahreaz@gmail.com>
2021-08-24 19:57:34 +0600

    Frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My book list</h1>
+    </div>
+  );
+}
+
+export default App;
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,6 +11,7 @@ const {
 	GraphQLID,
 	GraphQLInt,
 	GraphQLList,
+	GraphQLNonNull,
 } = graphgl;
 
 // Types
@@ -84,8 +85,8 @@ const Mutation = new GraphQLObjectType({
 		addAuthor: {
 			type: AuthorType,
 			args: {
-				name: { type: GraphQLString },
-				age: { type: GraphQLInt },
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) },
 			},
 			resolve(parent, args) {
 				const author = new Author({
@@ -98,9 +99,9 @@ const Mutation = new GraphQLObjectType({
 		addBook: {
 			type: BookType,
 			args: {
-				name: { type: GraphQLString },
-				genre: { type: GraphQLString },
-				authorId: { type: GraphQLID },
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				genre: { type: new GraphQLNonNull(GraphQLString) },
+				authorId: { type: new GraphQLNonNull(GraphQLID) },
 			},
 			resolve(parent, args) {
 				const book = new Book(args);
